5ab2028374c6fcb804090614889874a18ba2393d
ntoichkina <ntoichkina@polecat.agency>
2023-04-07 13:45:31 +0300

    Update pull_dependencies.sh

--- Scripts/pull_dependencies.sh
+++ Scripts/pull_dependencies.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-git subtree pull --prefix Sources/OpenVPN3 git@github.com:OpenVPN/openvpn3.git --squash
+OPENVPN3_VERSION="release/3.7.2"
 
-git subtree pull --prefix Sources/ASIO git@github.com:chriskohlhoff/asio.git --squash
-git subtree pull --prefix Sources/LZ4 git@github.com:lz4/lz4.git --squash
-git subtree pull --prefix Sources/mbedTLS git@github.com:ARMmbed/mbedtls.git --squash
+git subtree pull --prefix Sources/OpenVPN3 git@github.com:OpenVPN/openvpn3.git ${OPENVPN3_VERSION} --squash
+
+source Sources/OpenVPN3/deps/lib-versions
+
+git subtree pull --prefix Sources/ASIO git@github.com:chriskohlhoff/asio.git ${ASIO_VERSION} --squash
+git subtree pull --prefix Sources/LZ4 git@github.com:lz4/lz4.git ${LZ4_VERSION/lz4-/v} --squash
+git subtree pull --prefix Sources/mbedTLS git@github.com:ARMmbed/mbedtls.git ${MBEDTLS_VERSION} --squash
