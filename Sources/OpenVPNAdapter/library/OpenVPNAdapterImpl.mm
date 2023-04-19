a13f695eb13eb9a459c68044abf17e6de6d504d8
ntoichkina <ntoichkina@polecat.agency>
2023-04-19 17:53:06 +0300

    Update OpenVPNAdapterImpl.mm

--- Sources/OpenVPNAdapter/library/OpenVPNAdapterImpl.mm
+++ Sources/OpenVPNAdapter/library/OpenVPNAdapterImpl.mm
@@ -449,7 +449,7 @@
     dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, TUNNEL_CONFIGURATION_TIMEOUT * NSEC_PER_SEC));
     
     if (configurationError) {
-        NSString *message = [NSString stringWithFormat:@"Failed to reset tunnel. Check underlying error for more details. disconnect = %@; configurationError %@", @(disconnect), configurationError];
+        NSString *message = [NSString stringWithFormat:@"Failed to reset tunnel. Check underlying error for more details. disconnect = %@; configurationError %@", @(disconnect), @(configurationError != NULL)];
         NSDictionary *userInfo = @{
             NSLocalizedDescriptionKey: message,
             NSUnderlyingErrorKey: configurationError,
