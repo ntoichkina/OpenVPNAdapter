313c90e4d066de92cee9071ebed72145a41c682a
Anatoliy Radchenko <anatox91@yandex.ru>
2023-04-28 18:38:20 +0300

    update pull_dependencies.sh script

--- Scripts/pull_dependencies.sh
+++ Scripts/pull_dependencies.sh
@@ -4,10 +4,10 @@ set -e
 
 OPENVPN3_VERSION="master"
 
-git subtree pull --prefix Sources/OpenVPN3 git@github.com:OpenVPN/openvpn3.git ${OPENVPN3_VERSION} --squash
+git subtree pull --prefix Sources/OpenVPN3 git@github.com:OpenVPN/openvpn3.git ${OPENVPN3_VERSION} --squash -m "update OpenVPN3"
 
 source Sources/OpenVPN3/deps/lib-versions
 
-git subtree pull --prefix Sources/ASIO git@github.com:chriskohlhoff/asio.git ${ASIO_VERSION} --squash
-git subtree pull --prefix Sources/LZ4 git@github.com:lz4/lz4.git ${LZ4_VERSION/lz4-/v} --squash
-git subtree pull --prefix Sources/mbedTLS git@github.com:ARMmbed/mbedtls.git ${MBEDTLS_VERSION} --squash
+git subtree pull --prefix Sources/ASIO git@github.com:chriskohlhoff/asio.git ${ASIO_VERSION} --squash -m "update ASIO"
+git subtree pull --prefix Sources/LZ4 git@github.com:lz4/lz4.git ${LZ4_VERSION/lz4-/v} --squash -m "update LZ4"
+git subtree pull --prefix Sources/mbedTLS git@github.com:ARMmbed/mbedtls.git ${MBEDTLS_VERSION} --squash -m "update mbedTLS"
